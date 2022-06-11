d57db81c66380d4847fe1ca2e545952fb5f75e37
ericmoraess <ericmoraess@gmail.com>
2022-06-11 15:17:23 -0300

    adicionando mais informacoes no atividade

---
+++ gabarito.sql
@@ -0,0 +1,10 @@
+insert into jogador
+values ('Mailson', 25, 1.97, 90);
+
+insert into jogador
+values ('Magrão', 35, 1.99, 110);
+
+
+
+insert into clube values('Sport Clube do Recife', 'Leonardo Lopes', '13/05/1905', 'https://sportrecife.com.br/');
+insert into clube values('Clube Náutico Capibaribe', 'Diógenes Braga', '07/04/1901', 'https://www.nautico-pe.com.br/');
