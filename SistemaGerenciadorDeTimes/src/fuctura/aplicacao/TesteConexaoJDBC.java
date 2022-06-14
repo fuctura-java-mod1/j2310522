4ca2bd5b952d21f30bce0c97233e9bebc499ecf8
ericmoraess <ericmoraess@gmail.com>
2022-06-14 20:31:44 -0300

    aula 3 - parcial

--- SistemaGerenciadorDeTimes/src/fuctura/aplicacao/TesteConexaoJDBC.java
+++ SistemaGerenciadorDeTimes/src/fuctura/aplicacao/TesteConexaoJDBC.java
@@ -3,20 +3,38 @@ package fuctura.aplicacao;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class TesteConexaoJDBC {
 	
-	public static void main(String[] args) { //veja se tem algo 
+	public static void main(String[] args) throws ClassNotFoundException { //veja se tem algo 
 		String url = "jdbc:oracle:thin:@localhost:1521:xe";
 		String usuario = "java2";
 		String senha = "123";
 		
 		try {
-			Connection c = DriverManager.getConnection(url, usuario, senha);
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			
+			Connection conexao = DriverManager.getConnection(url, usuario, senha);
+			
+			inserir(conexao);
+			
+			System.out.println("Jogador inserido com sucesso!");
+			
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 		
 		System.out.println("Conectado com sucesso!");
+	}
+	
+	public static void inserir(Connection conexao) {
+		try {
+			Statement smt = conexao.createStatement();
+			String comandoSql = "INSERT INTO jogador VALUES ('Ciro', 1.80, 70.0, 29)";
+			smt.execute(comandoSql);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
 }
