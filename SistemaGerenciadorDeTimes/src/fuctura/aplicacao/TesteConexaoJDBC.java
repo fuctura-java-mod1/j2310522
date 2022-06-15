193300af5c4eb4a83456bafacadae917390cad9e
ericmoraess <ericmoraess@gmail.com>
2022-06-14 21:15:05 -0300

    inserir com PreparedStatement

--- SistemaGerenciadorDeTimes/src/fuctura/aplicacao/TesteConexaoJDBC.java
+++ SistemaGerenciadorDeTimes/src/fuctura/aplicacao/TesteConexaoJDBC.java
@@ -2,8 +2,11 @@ package fuctura.aplicacao;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.Statement;
+
+import fuctura.model.Jogador;
 
 public class TesteConexaoJDBC {
 	
@@ -13,11 +16,17 @@ public class TesteConexaoJDBC {
 		String senha = "123";
 		
 		try {
+			
 			Class.forName("oracle.jdbc.driver.OracleDriver");
 			
 			Connection conexao = DriverManager.getConnection(url, usuario, senha);
 			
-			inserir(conexao);
+			Jogador j = new Jogador();
+			j.setNome("Messi");
+			j.setAltura(1.90);
+			
+			//inserir(conexao, j);
+			inserirComPreparedStatement(conexao, j);
 			
 			System.out.println("Jogador inserido com sucesso!");
 			
@@ -28,13 +37,32 @@ public class TesteConexaoJDBC {
 		System.out.println("Conectado com sucesso!");
 	}
 	
-	public static void inserir(Connection conexao) {
+	public static void inserir(Connection conexao, Jogador j) {
 		try {
 			Statement smt = conexao.createStatement();
-			String comandoSql = "INSERT INTO jogador VALUES ('Ciro', 1.80, 70.0, 29)";
+			String comandoSql = "INSERT INTO jogador VALUES (" + j.getNome() + "," + j.getAltura() + "," + j.getPeso() + "," +  j.getIdade() + ")";
 			smt.execute(comandoSql);
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 	}
+	
+	public static void inserirComPreparedStatement(Connection conexao, Jogador j) {
+		try {
+			                                               //1, 2  3  4
+			String comandoSql = "INSERT INTO jogador VALUES (?, ?, ?, ?)";
+			PreparedStatement psmt = conexao.prepareStatement(comandoSql);
+				
+			psmt.setInt(4, j.getIdade());
+			psmt.setString(1, j.getNome()); 
+			psmt.setDouble(3, j.getPeso());
+			psmt.setDouble(2, j.getAltura());
+			
+			psmt.execute();
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
 }
