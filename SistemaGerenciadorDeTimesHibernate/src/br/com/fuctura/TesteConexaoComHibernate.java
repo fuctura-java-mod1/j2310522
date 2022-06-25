358db006c1125b832774be4b84ae524f97bc93cf
ericmoraess <ericmoraess@gmail.com>
2022-06-25 03:24:36 -0300

    erro de transação

--- SistemaGerenciadorDeTimesHibernate/src/br/com/fuctura/TesteConexaoComHibernate.java
+++ SistemaGerenciadorDeTimesHibernate/src/br/com/fuctura/TesteConexaoComHibernate.java
@@ -11,6 +11,7 @@ import javax.persistence.metamodel.Metamodel;
 import javax.swing.JTextArea;
 
 import br.com.fuctura.entity.Jogador;
+import br.com.fuctura.entity.Tecnico;
 
 public class TesteConexaoComHibernate {
 	
@@ -19,6 +20,7 @@ public class TesteConexaoComHibernate {
 		EntityManager em = null;
 		
 		try {
+			
 			fabrica = Persistence.createEntityManagerFactory("Fuctura-PU");
 			em = fabrica.createEntityManager();
 			Metamodel meta  = em.getMetamodel();
@@ -29,11 +31,24 @@ public class TesteConexaoComHibernate {
 			j.setNome("Pelé");
 			j.setAltura(1.89);
 			
+			Tecnico t1 = new Tecnico();
+			t1.setCodigo(1);
+			t1.setNome("Felipao");
+			
+			Tecnico t2 = new Tecnico();
+			t2.setCodigo(1);
+			t2.setNome("Zagalo");
+			
+			
 			EntityTransaction tx = em.getTransaction();
 			
 			System.out.println("-- Início da transação --");
 			tx.begin();
+			
 			em.persist(j);
+			em.persist(t1);
+			em.persist(t2);
+			
 			tx.commit();
 			System.out.println("-- Fim da transação --");
 			
@@ -42,7 +57,6 @@ public class TesteConexaoComHibernate {
 			}else {
 				System.out.println("Não está gerenciando: " + j.toString());
 			}
-			
 			
 		}finally {
 			em.close();
---
+++ SistemaGerenciadorDeTimesHibernate/src/br/com/fuctura/entity/Tecnico.java
@@ -0,0 +1,25 @@
+package br.com.fuctura.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_tecnico")
+public class Tecnico {
+	@Id
+	private Integer codigo;
+	private String nome;
+	public Integer getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(Integer codigo) {
+		this.codigo = codigo;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+}
